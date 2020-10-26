25eafd2624f103404b1a821c5d323f4155681876
iliabezh@gmail.com <iliabezh@gmail.com>
2020-10-26 18:25:49 +0300

    Изменения: -Правка названия таблиц в скриптах

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,9 @@ insert into users (login, password, phone_number) values
    ('login1','password1', '12343')
 ,  ('login2','password2', '12344');
 
-insert into card(dtype, balance, card_number, currency, limit_, used_limit, user_id) VALUES
+insert into cards(dtype, balance, card_number, currency, limit_, used_limit, user_id) VALUES
  ('CreditCard', 1000, '5264', 'RUB', 0, 0 , 1);
 
-INSERT INTO payment (card_number, pay_sum, paymentuid, status, user_id) VALUES
+INSERT INTO payments (card_number, pay_sum, paymentuid, status, user_id) VALUES
 ( '5264', 10, 'paymentuid1', 'SUCCESS', 1);
 
